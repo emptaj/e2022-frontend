63403affd3e3f182a7ebdf0493fb9cae6133a56d
Marcin Szyszka <toja@example.com>
2022-05-27 22:45:11 +0200

    Setup login page: styles

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,32 @@
+import { Grid, Paper, styled, Avatar } from '@mui/material'
 import React from 'react'
+import LoginForm from '../components/LoginForm'
 
 function Login() {
+    const StyledPaper = styled(Paper)({
+        padding: "20px",
+        height: "70vh",
+        width: 400,
+        margin: "20px auto"
+
+    })
+
+    const StyledAvatar = styled(Avatar)({
+        backgroundColor: "#ff5722"
+    })
+
     return (
         <div>
             <h1>Login page</h1>
-        </div>
+            <Grid
+                align="center"
+            >
+                <StyledPaper elevation={10}>
+                    <StyledAvatar>E22</StyledAvatar>
+                    <LoginForm />
+                </StyledPaper>
+            </Grid>
+        </div >
     )
 }
 
