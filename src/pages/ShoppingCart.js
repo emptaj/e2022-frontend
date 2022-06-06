b3e4851bf7d931ade5b3a416ea0d854fd9fb42f0
Jakub Szlezak <jakub-szlezak@wp.pl>
2022-06-06 19:09:32 +0200

    Changed cart to array of objects, no mapping needed

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -3,31 +3,10 @@ import ProductDetails from "../components/ProductDetails";
 
 export default function ShoppingCart({cartItems, setCartItems}) {
 
-    console.log(cartItems);
-    let cartMap = new Map();
-    cartItems.forEach(item => {
-        if(cartMap.has(item)) 
-            cartMap.set(item, cartMap.get(item) + 1);
-        else
-            cartMap.set(item, 1);
-        console.log(item);
-    })
-
-    
-    //====================================== DO ZMIANY
-    let items = []
-
-    for(const [itemStr, quantity] of cartMap.entries()){
-        const item = JSON.parse(itemStr)
-        items.push(<div> <ProductDetails {...item } setCartItems={setCartItems} addOrDelete={false}/>  {quantity} </div>)
-     }
-    //======================================
-
-
     return (
         <div>
             <h1> Shopping cart </h1>
-            {items}
+            {cartItems && cartItems.map(product =><div> <ProductDetails { ...JSON.parse(product.item) } setCartItems={setCartItems} addOrDelete={false} /> {product.quantity} </div> )}
         </div>
     )
 }
