85f52e399ee07616d68cb59a945bc3b05abedd39
Jakub Szlezak <jakub-szlezak@wp.pl>
2022-06-06 11:30:02 +0200

    Divided registrationComponent into Form and Component

--- src/components/SignUpComponent.js
+++ src/components/SignUpComponent.js
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import { useState, useEffect, useRef } from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
+import SignUpForm from './SingUpForm';
 
 const emptyErrorMsg = {
   username: '',
@@ -17,22 +11,10 @@ const emptyErrorMsg = {
 }
 
 export default function SignUpComponent() {
+  const [signUpData, setSignUpData] = useState({username: '', email: '', password: '', confPassword: ''})
   const [errorMsg, setErrorMsg] = useState(emptyErrorMsg);
-  const [requestData, setRequestData] = useState({});
-  const isMounted = useRef(false);
-  
-  useEffect(() =>
-    {
-      if(!isMounted.current){
-        isMounted.current = true;
-        return;
-      }
-      if(checkPasswords())
-        registerUser();
-    }, [requestData])
 
   async function registerUser() {
-    console.log(requestData)
     const response = await fetch('http://localhost:8080/api/users/', {
         method: 'POST',
         headers: {
@@ -40,110 +22,41 @@ export default function SignUpComponent() {
           'Content-Type': 'application/json'
         },
         body:JSON.stringify({
-                  username: requestData.username,
-                  email: requestData.email,
-                  password: requestData.password
+                  username: signUpData.username,
+                  email: signUpData.email,
+                  password: signUpData.password
                 })
       }).catch(err => console.log(err));
-      
-      const data = await response.json();
-      console.log(data);
-      if(response.status === 400)
-        setErrorMsg(data);
-    }
-  
-    function checkPasswords() {
-      if ( !requestData.password || requestData.password !== requestData.confPassword )
-      {
-        setErrorMsg({ confPassword: "Passwords are different!" });
-        return false;
-      }
-      return true;
-    }
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
-      setRequestData({
-        username: data.get('username'),
-        email: data.get('email'),
-        password: data.get('password'),
-        confPassword: data.get('confirmationPassword')
-      })
-    };
+    
+    const data = await response.json();
+    console.log(data);
+    if(response.status === 400)
+      setErrorMsg(data);
+  }
 
-    return (
-      <Box component="form" onSubmit={ handleSubmit } sx={{ mt: 3 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <TextField
-            autoFocus
-            required
-            fullWidth
-            id="username"
-            name="username"
-            label="Username"
-            error= { !!errorMsg.username || !!errorMsg.message }
-            helperText= { errorMsg.username }
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            fullWidth
-            id="email"
-            name="email"
-            label="Email Address"
-            error= { !!errorMsg.email || !!errorMsg.message }
-            helperText= { errorMsg.email }
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            fullWidth
-            id="password"
-            name="password"
-            label="Password"
-            type="password"
-            error= { !!errorMsg.password || !!errorMsg.message }
-            helperText= { errorMsg.password }
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            fullWidth
-            id="confirmationPassword"
-            name="confirmationPassword"
-            label="Confirm password"
-            type="password"
-            error= { !!errorMsg.confPassword || !!errorMsg.message }
-            helperText= { !!errorMsg.confPassword? errorMsg.confPassword : errorMsg.message }
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox value="allowExtraEmails" color="primary" />}
-            label="I want to receive inspiration, marketing promotions and updates via email."
-          />
-        </Grid>
-      </Grid>
-      <Button
-        fullWidth
-        type="submit"
-        variant="contained"
-        sx={{ mt: 3, mb: 2 }}
-      >
-        Sign up
-      </Button>
-      <Grid container justifyContent="center">
-        <Grid item >
-          <Link href="#" variant="body2" >
-           Already have an account? Sign in
-          </Link>
-        </Grid>
-      </Grid>
-    </Box>
-    )
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+    setSignUpData((prevData) => {
+      return { 
+        ...prevData,
+        [name]: value
+      }
+    })
+  }
+
+  useEffect(() => {
+    if(!errorMsg.useEffect || !Object.values(errorMsg).every(x => !x))
+      return;
+    registerUser();
+  }, [errorMsg]);
+
+  const handleSubmit = () => {
+    console.log("DATA FOR SERVER: " + JSON.stringify(signUpData))
+    registerUser();
+  };
+
+  return (
+    <SignUpForm formData={signUpData} onChangeFunction={handleChange} onSubmitFunction={handleSubmit} 
+      errorMsg={errorMsg} setErrorMsg={setErrorMsg} />
+  )
 }
---
+++ src/components/SingUpForm.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { validateConfPassword, validateEmail, validatePassword, validateUsername } from '../constants/Validators';
+
+function SignUpForm({ formData, onChangeFunction, errorMsg, setErrorMsg }) {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setErrorMsg({
+      username: validateUsername(formData.username),
+      email: validateEmail(formData.email),
+      password: validatePassword(formData.password),
+      confPassword: validateConfPassword(formData.password, formData.confPassword),
+      useEffect: true
+    });
+  }
+
+  return (
+    <Box component="form" onSubmit={ handleSubmit } sx={{ mt: 3 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            autoFocus
+            required
+            fullWidth
+            id="username"
+            name="username"
+            label="Username"
+            value={formData.username}
+            onChange={event => onChangeFunction(event)}
+            error= { !!errorMsg.username || !!errorMsg.message }
+            helperText= { errorMsg.username }
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="email"
+            name="email"
+            label="Email Address"
+            value={formData.email}
+            onChange={event => onChangeFunction(event)}
+            error= { !!errorMsg.email || !!errorMsg.message }
+            helperText= { errorMsg.email }
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            value={formData.password}
+            onChange={event => onChangeFunction(event)}
+            error= { !!errorMsg.password || !!errorMsg.message }
+            helperText= { errorMsg.password }
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="confPassword"
+            name="confPassword"
+            label="Confirm password"
+            type="password"
+            value={formData.confPassword}
+            onChange={event => onChangeFunction(event)}
+            error= { !!errorMsg.confPassword || !!errorMsg.message }
+            helperText= { !!errorMsg.confPassword? errorMsg.confPassword : errorMsg.message }
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox value="allowExtraEmails" color="primary" />}
+            label="I want to receive inspiration, marketing promotions and updates via email."
+          />
+        </Grid>
+      </Grid>
+      <Button
+        fullWidth
+        type="submit"
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+      >
+        Sign up
+      </Button>
+      <Grid container justifyContent="center">
+        <Grid item >
+          <Link href="#" variant="body2" >
+            Already have an account? Sign in
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+export default SignUpForm;
