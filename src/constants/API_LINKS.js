c6dd2681180910ab1edb7648396c31c4e1f5233a
Jakub Szlezak <jakub-szlezak@wp.pl>
2022-06-07 18:31:16 +0200

    Fixing typo contnuation

--- src/constants/API_LINKS.js
+++ src/constants/API_LINKS.js
@@ -5,7 +5,7 @@ export const STATIC_LINKS = {
     PRODUCTS: BASE_LINK+"/products",
     REGISTER: BASE_LINK+"/users",
     CREATE_ORDER: BASE_LINK + "/orders",
-    DELIEVERY_TYPES: BASE_LINK + "/delivery-types"
+    DELIVERY_TYPES: BASE_LINK + "/delivery-types"
 }
 
 export const API_LINK_WAREHOUSES_ID_PRODUCTS = (id) => BASE_LINK + "/warehouses/" + id + "/products";
