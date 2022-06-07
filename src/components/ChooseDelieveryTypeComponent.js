311b2d95710b053be6263768a9dc1eab1c93bfbc
Jakub Szlezak <jakub-szlezak@wp.pl>
2022-06-07 14:42:16 +0200

    Place your order - user can choose delieveryType

---
+++ src/components/ChooseDelieveryTypeComponent.js
@@ -0,0 +1,25 @@
+import { Card, CardContent, Typography } from "@mui/material";
+import GridItem from "@mui/material/Grid";
+import React, { useState } from "react";
+import { STATIC_LINKS } from "../constants/API_LINKS";
+
+export default function ChooseDelieveryTypeComponent( {id, name, email, addressId, setCartItems} ) {
+
+    return(
+        <GridItem md={4} xs={12}>
+            <Card onClick={() => setCartItems(id)}>
+                <CardContent>
+                    <Typography gutterBottom>
+                      {email}
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        {name}
+                    </Typography>
+                    <Typography>
+                        {id}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </GridItem >
+    )
+}
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,14 +1,18 @@
 import { Tab, Tabs } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import ChooseDelieveryTypeComponent from "../components/ChooseDelieveryTypeComponent";
 
 import ShoppingCartComponent from "../components/ShoppingCartComponent";
+import WithListHOC from "../components/WithListHOC";
+import { STATIC_LINKS } from "../constants/API_LINKS";
 
 export default function ShoppingCart({cartItems, setCartItems}) {
-    const [value, setValue] = useState(0);
+    const [tableValue, setTableValue] = useState(0);
+    const [deliveryTypeId, setDelieveryTypeId] = useState(0);
 
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
+    const handleTabChange = (event, newValue) => {
+        setTableValue(newValue);
     };
 
     return (
@@ -16,8 +20,8 @@ export default function ShoppingCart({cartItems, setCartItems}) {
             <h1> Place your order </h1>
             <Box>
                 <Tabs
-                    value={value}
-                    onChange={handleChange}
+                    value={tableValue}
+                    onChange={handleTabChange}
                     variant="scrollable"
                     scrollButtons={false}
                     aria-label="scrollable prevent tabs example"
@@ -28,7 +32,20 @@ export default function ShoppingCart({cartItems, setCartItems}) {
                     <Tab label="Confirm" />
                 </Tabs>
             </Box>
-            <ShoppingCartComponent cartItems={cartItems} setCartItems={setCartItems}/>
+            {(() => {
+                switch (tableValue) {
+                    case 0: 
+                        return <ShoppingCartComponent cartItems={cartItems} setCartItems={setCartItems}/>;
+                    case 1:
+                        return (
+                            <div> 
+                                <h1>Chosen delievery id: {deliveryTypeId? deliveryTypeId : 'None'}</h1> <WithListHOC WrappedComponent={ChooseDelieveryTypeComponent} API_LINK={STATIC_LINKS.DELIEVERY_TYPES} setCartItems={setDelieveryTypeId} /> 
+                            </ div>
+                        )
+                }
+            })()}
+            {/* <ShoppingCartComponent cartItems={cartItems} setCartItems={setCartItems}/> */}
+            {/* <WithListHOC WrappedComponent={ChooseDelieveryTypeComponent} API_LINK={STATIC_LINKS.DELIEVERY_TYPES} /> */}
         </div>
     );
 }
