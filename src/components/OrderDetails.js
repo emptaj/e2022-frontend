d2105f5e36c2afaa55139c859b181117f02324a5
Marcin Szyszka <szyszka.m98@gmail.com>
2022-06-17 18:19:24 +0200

    Fetching order status

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -3,19 +3,19 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Button, Card, CardContent, Typography } from '@mui/material';
 import GridItem from "@mui/material/Grid";
 import CardActions from '@mui/material/CardActions';
-import { API_LINK_ADDRESS_ID } from '../constants/API_LINKS';
+import { API_LINK_ADDRESS_ID, API_LINK_REFRESH_ORDER_STATUS } from '../constants/API_LINKS';
 import { DETAIL_CARD_STYLE } from '../constants/Styles';
 import refreshToken from '../constants/RefreshToken';
 
 
-export default function OrderDetails( { id, addressId, state } ) {
-    const API_LINK= API_LINK_ADDRESS_ID(addressId);
+export default function OrderDetails ({ id, addressId, state }) {
+    const API_LINK = API_LINK_ADDRESS_ID(addressId);
     const [address, setAddress] = useState({});
     const navigate = useNavigate();
 
     const cardStyle = DETAIL_CARD_STYLE;
 
-    async function getAddress() {
+    async function getAddress () {
         let response, data;
         response = await fetch(API_LINK, {
             headers: {
@@ -23,23 +23,36 @@ export default function OrderDetails( { id, addressId, state } ) {
             }
         }).catch(err => console.log(err));
 
-        if(response.status === 401) {
+        if (response.status === 401) {
             navigate('/login');
         }
 
         data = await response.json();
 
-        if(response.status === 403 && data.error_message.includes("The Token has expired")){
+        if (response.status === 403 && data.error_message.includes("The Token has expired")) {
             data = await refreshToken(getAddress, null);
         }
-        
-        
+
+
         return data;
     }
 
+    async function refreshState () {
+        const response = await fetch(API_LINK_REFRESH_ORDER_STATUS(id), {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('access_token')
+            }
+        }
+        ).catch(err => console.log(err));
+    }
+
     useEffect(() => {
+        refreshState()
         getAddress().then(data => setAddress(data));
-    },[addressId])
+    }, [addressId])
 
     return (
         <GridItem md={4} xs={12}>
@@ -49,9 +62,9 @@ export default function OrderDetails( { id, addressId, state } ) {
                         {state}
                     </Typography>
                     <Typography variant="h5" component="div">
-                        {address.city}   
+                        {address.city}
                     </Typography>
-                    <Typography variant="body2"> 
+                    <Typography variant="body2">
                         {address.street} {address.houseNum}/{address.flatNum}
                     </Typography>
                 </CardContent>
